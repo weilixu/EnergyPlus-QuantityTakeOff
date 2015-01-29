b76db5112a8f07062528f1c7f1a1dbc01448ba71
Adrian Chong <Adrian@adrians-mbp-2.wv.cc.cmu.edu>
2015-01-29 14:44:18 -0500

    working readCSV

--- EnergyPlusUncertaintyAnalyzer/src/analyzer/readcsv/testReadCSV.java
+++ EnergyPlusUncertaintyAnalyzer/src/analyzer/readcsv/testReadCSV.java
@@ -10,20 +10,8 @@ public class testReadCSV {
 		ReadCSV readCSV = new ReadCSV();
 
 		String filename = "/Users/Adrian/Dropbox/testIDFJfreeChart/testCSV.csv";
-		ArrayList<String> data = readCSV.readData(filename);
-//		System.out.println(Arrays.toString(myData));
-		int len = data.size();
-		String[] output = new String[len];
-		for (int i=0; i<len; i++){
-			
-			output[i] = data.get(i);
-//			System.out.println(output[i]);
-//			System.out.println(Double.parseDouble(data.get(i)));
-		}
-		System.out.print(output[5]);
-//		for (int j=0; j<output.length; j++){
-//			System.out.println(Double.parseDouble(output[j]));
-//		}
+		double[] data = readCSV.readData(filename);
+		System.out.println(Arrays.toString(data));
 		
 
 	}
