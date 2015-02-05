645ad7652f1cfc63b4785feda2e371c5b11c8161
Adrian Chong <Adrian@Adrians-MacBook-Pro-2.local>
2015-02-05 13:17:48 -0500

    added comments for AnalyzeResult

--- EnergyPlusUncertaintyAnalyzer/src/analyzer/graphs/AnalyzeResult.java
+++ EnergyPlusUncertaintyAnalyzer/src/analyzer/graphs/AnalyzeResult.java
@@ -20,6 +20,17 @@ public class AnalyzeResult {
 	private int numberofResults;
 	private String runSizing;
 
+	/**
+	 * Constructor to initialize values
+	 * 
+	 * @param source
+	 *                path to source directory
+	 * @param idfName
+	 *                name of energyplus file without .idf
+	 * @param rs
+	 *                String indicating if runsizing is turned on/off in
+	 *                EnergyPlus file
+	 */
 	public AnalyzeResult(String source, String idfName, String rs) {
 		this.runSizing = rs;
 		this.keys = new ArrayList<String>();
@@ -29,23 +40,48 @@ public class AnalyzeResult {
 		this.idfName = idfName;
 		this.startYear = Calendar.getInstance().get(Calendar.YEAR);
 	}
-	
-	public void setStartYear(int idfYear){
+
+	/**
+	 * set year to plot timeseries graph
+	 * 
+	 * @param idfYear
+	 *                starting year of simulation
+	 */
+	public void setStartYear(int idfYear) {
 		startYear = idfYear;
 	}
 
+	/**
+	 * return month which acts as a key to hashmap containing the data
+	 */
 	public String getKey(int index) {
 		return keys.get(index);
 	}
-	
+
+	/**
+	 * @return number of valid keys in data
+	 */
 	public int getKeysLength() {
 		return keys.size();
 	}
-	
+
+	/**
+	 * 
+	 * @return starting year of simulation
+	 */
 	public int getStartYear() {
 		return startYear;
 	}
 
+	/**
+	 * @param key
+	 *                month for which data would be extracted for
+	 * @param colNumber
+	 *                index indicating the variable for the data to extract
+	 *                from
+	 * @return an array containing all data from simulation for month i
+	 *         (given by key) and variable j (given by colNumber)
+	 */
 	public double[] getData(String key, int colNumber) {
 		ArrayList<Double[]> list = this.data.get(key);
 		int length = list.size();
@@ -56,32 +92,56 @@ public class AnalyzeResult {
 		}
 		return output;
 	}
-	
+
+	/**
+	 * Sums all monthly data for variable indicated by colNumber
+	 * 
+	 * @param colNumber
+	 *                index indicating the variable for the data to extract
+	 *                from
+	 * @return an array containing consumption for runperiod (indicated in
+	 *         EnergyPlus) for variable j (given by colNumber)
+	 */
 	public double[] getHistogramData(int colNumber) {
 		double[] output = new double[numberofResults];
 		System.out.println(output.length);
 		System.out.println(colNumber);
-		for (int i=0; i<keys.size(); i++){
+		for (int i = 0; i < keys.size(); i++) {
 			String month = keys.get(i);
 			ArrayList<Double[]> monthData = this.data.get(month);
-			for (int j=0; j<monthData.size(); j++){
-			    System.out.println("This is the month data size: " +monthData.size());
+			for (int j = 0; j < monthData.size(); j++) {
+				System.out.println("This is the month data size: "
+						+ monthData.size());
 				Double[] temp = monthData.get(j);
 				output[j] = output[j] + temp[colNumber];
 			}
 		}
-		return output;		
-	}
-	
-	public String getVariable(int index){
-		return header[index];
-	}
-	
-	public int getVariableLength(){
-		return varLength;
-//		return header.length;
+		return output;
 	}
 
+	/**
+	 * 
+	 * @param index
+	 *                indicates position of variable in header
+	 * @return name of variable at position index
+	 */
+	public String getVariable(int index) {
+		return header[index];
+	}
+
+	/**
+	 * 
+	 * @return number of valid variables
+	 */
+	public int getVariableLength() {
+		return varLength;
+		// return header.length;
+	}
+
+	/**
+	 * set the list of valid variables by reading the first meter.csv file.
+	 * needs to be called before any get methods are used.
+	 */
 	public void setHeader() {
 		FileReader file;
 		String filename = this.source + this.idfName + "0Meter.csv";
@@ -108,8 +168,16 @@ public class AnalyzeResult {
 		}
 	}
 
-
-
+	/**
+	 * saves data into hashmap by reading all Meter.csv files. HashMap is of
+	 * format HashMap<String, ArrayList<Double[]>>. where key is the month
+	 * for which data is stored and each Double[] refers to data for each
+	 * simulation for that month. Hence ArrayList<Double[]> contains all
+	 * data for a particular month which is stored in keys
+	 * 
+	 * @param numSim
+	 *                number of simulation runs
+	 */
 	public void setData(int numSim) {
 		// HashMap<String, ArrayList<Double[]>> data = new
 		// HashMap<String, ArrayList<Double[]>>();
@@ -125,9 +193,10 @@ public class AnalyzeResult {
 				br.readLine();
 				if (runSizing.equalsIgnoreCase("Yes")) {
 					br.readLine();
-					br.readLine(); // skip first 3 lines (header)
+					br.readLine(); // skip first 3 lines
+							// (header)
 				}
-				
+
 				Double[] tempData = null;
 				while ((line = br.readLine()) != null) {
 					String[] tempLine = line
@@ -151,7 +220,7 @@ public class AnalyzeResult {
 					}
 
 				}
-				
+
 				br.close();
 				numberofResults++;
 			} catch (FileNotFoundException e) {
@@ -166,17 +235,22 @@ public class AnalyzeResult {
 		// return data;
 	}
 
+	/**
+	 * convert string to double and where it is empty, saves as 0
+	 */
 	private Double[] convertStringToDouble(String[] args) {
 		int len = args.length;
 		Double[] output = new Double[len - 1];
 		for (int i = 0; i < len - 1; i++) {
 			// ignores 1st value of args which is the month string
 			// output should be all numeric data
-		    if(args[i+1].equals("")){
-			output[i]=0.0;
-		    }else{
-			output[i] = Double.parseDouble(args[i + 1])*2.7777777778e-7; // convert to kWh
-		    }
+			if (args[i + 1].equals("")) {
+				output[i] = 0.0;
+			} else {
+				output[i] = Double.parseDouble(args[i + 1]) * 2.7777777778e-7; // convert
+												// to
+												// kWh
+			}
 		}
 		return output;
 	}
