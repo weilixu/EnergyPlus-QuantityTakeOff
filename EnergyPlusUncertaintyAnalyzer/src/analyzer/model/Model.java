abd5745da135a52f3b32c72aa663e84f6e9a168a
weilixu <weilix@andrew.cmu.edu>
2015-02-12 09:01:14 -0500

    fix make distribution udpate

--- EnergyPlusUncertaintyAnalyzer/src/analyzer/model/Model.java
+++ EnergyPlusUncertaintyAnalyzer/src/analyzer/model/Model.java
@@ -35,30 +35,30 @@ public class Model {
     // Strings for the file names
     private final String DIST_NAME = "DIST_";
     private final String IMAGE_POST = ".jpg";
-    
+
     private String variableName;
 
     // record the number of the simulation to determine the size of data
     private int simulationNumber;
-    
+
     /*
      * Folders
      */
     private File eplusFile;
     private File parentFile;
     private File resultFile;
-    
+
     /*
      * All about make distribution model
      */
     private final MakeDistributionModel makeDistModel;
     private DistributionType type;
-    
+
     /**
      * idf reader <link>IdfReader<link>
      */
     private final IdfReader idfData;
-    
+
     /**
      * graph generator <link>GraphGenerator<link>
      */
@@ -67,18 +67,17 @@ public class Model {
      * <link>RunEnergyPlus<link>
      */
     private RunEnergyPlus run;
-    
+
     /**
      * fit distribution generator
      */
     private RVGenerator generator;
-    
+
     /**
      * sensitivity analysis module
      */
     private SensitivityAnalysis sensitivity;
-    
-    
+
     /*
      * A data structure to save generated random variables from the model. The
      * size of the double[] array is equal to the simulaitonNumber. String
@@ -107,7 +106,7 @@ public class Model {
 	idfData = new IdfReader();
 	run = new RunEnergyPlus();
 	generator = new RVGenerator();
-	
+
 	makeDistModel = new MakeDistributionModel();
 	loadIDFListeners = new ArrayList<LoadIdfListeners>();
 	distGeneListeners = new ArrayList<DistGenerationListeners>();
@@ -136,29 +135,29 @@ public class Model {
     public void addFitDistListeners(FitDistListeners f) {
 	fitDistListeners.add(f);
     }
-    
+
     /**
      * add the listener <link>MakeDistDisplayPanel<link>
+     * 
      * @param mdL
      */
     public void addMakeDistGraphGeneratorListener(
 	    MakeDistGraphGeneratorListener mdL) {
 	mdGraphs.add(mdL);
     }
-    
+
     public void addLoadIDFListeners(LoadIdfListeners l) {
 	loadIDFListeners.add(l);
     }
-    
+
     /**
      * add GUI listener to listen this class
+     * 
      * @param g
      */
     public void addGraphGenerationListener(GraphGenerationListener g) {
 	graphListeners.add(g);
     }
-    
-    
 
     /**
      * get the size of the data structure.
@@ -168,20 +167,13 @@ public class Model {
     public int getGeneratedVariableSize() {
 	return randomVariableList.size();
     }
-    
-    public File getResultFolder(){
+
+    public File getResultFolder() {
 	return resultFile;
     }
-    
-    public void initializeModel(File file) throws IOException{
-	eplusFile = file;
-	parentFile = eplusFile.getParentFile();
-	resultFile = createResultsFolder();
-	run.setFolder(resultFile);
-	
-	initializedIdfData();
-	
-	initializedGraphGenerator();
+
+    public HashMap<String, double[]> getData() {
+	return randomVariableList;
     }
 
     /**
@@ -193,54 +185,67 @@ public class Model {
 	simulationNumber = number;
 	makeDistModel.setSimulationNumber(simulationNumber);
     }
-    
-    public void setDistributionType(DistributionType type){
+
+    public void setDistributionType(DistributionType type) {
 	makeDistModel.setDistributionType(type);
     }
-    
-    public void setVariable(String variable){
+
+    public void setVariable(String variable) {
 	variableName = variable;
 	generator.setVariableName(variableName);
     }
-    
+
+    public void initializeModel(File file) throws IOException {
+	eplusFile = file;
+	parentFile = eplusFile.getParentFile();
+	resultFile = createResultsFolder();
+	run.setFolder(resultFile);
+	initializedIdfData();
+	initializedGraphGenerator();
+    }
+
     /**
      * start running EnergyPlus simulation
+     * 
      * @param eplus
      * @param weather
      * @param numberProc
      * @throws Exception
      */
-    public void startSimulation(String eplus, String weather, String numberProc) throws Exception{
+    public void startSimulation(String eplus, String weather, String numberProc)
+	    throws Exception {
 	run.setEnergyPlusDirectory(eplus);
 	run.setWeatherFile(weather);
 	run.setNumberOfProcessor(numberProc);
-	
-	try{
+
+	try {
 	    run.startSimulation();
-	}catch(Exception e){
+	} catch (Exception e) {
 	    throw e;
 	}
     }
-    
-    public void initializeSenstivityAnalysis(){
+
+    public void initializeSenstivityAnalysis() {
 	ArrayList<String> variableList = idfData.getVariableList();
 	String[] vn = new String[variableList.size()];
-	for(int i=0; i<variableList.size(); i++){
+	for (int i = 0; i < variableList.size(); i++) {
 	    vn[i] = variableList.get(i);
 	}
-	sensitivity = new SensitivityAnalysis(vn,graphs.getResults(), randomVariableList,graphs.getMissingList());
+	sensitivity = new SensitivityAnalysis(vn, graphs.getResults(),
+		randomVariableList, graphs.getMissingList());
 	double[] results = sensitivity.getCorrelation();
 	System.out.println(Arrays.toString(results));
     }
-    
-    public void writeIdfFile(File targetFolder){
-	for(int i = 0; i<simulationNumber; i++){
-	    writingFile(idfData.cloneIdfData(),i, targetFolder);
-	}
-    }
 
-    public HashMap<String, double[]> getData() {
-	return randomVariableList;
+    /**
+     * Write the idf file to the result folder
+     * 
+     * @param targetFolder
+     */
+    public void writeIdfFile(File targetFolder) {
+	for (int i = 0; i < simulationNumber; i++) {
+	    writingFile(idfData.cloneIdfData(), i, targetFolder);
+	}
     }
 
     /**
@@ -252,36 +257,26 @@ public class Model {
     }
 
     /**
+     * Fit the CSV data into a distribution
      * 
-     * @param source
-     *            Directory to save image to
-     * @param imgName
-     *            Name of file to save image to (e.g. "testImage.jpg")
      * @param data
-     *            data to fit distribution to
-     * @param numRV
-     *            number of random variables to generate
+     *            : data extracted from a CSV file
      * @param sortby
-     *            sortby is the likelihood function used to sort the fitted
-     *            probability distribtuions. Valid inputs are: NLogL - Negative
-     *            Log Likelihood BIC - Bayesian Information criterion AIC -
-     *            Akaike information criterion AICc
+     *            : sorting criteria
      * @param dataType
-     *            dataType specifies whether to fit a 'DISCRETE' or 'CONTINUOUS'
-     *            distribution
+     *            : continuous or discrete
      * @param lower
-     *            lower bound where the generated random variables will be
-     *            truncated tor
+     *            : truncation lower bound
      * @param upper
-     *            upper bound where the generated random variables will be
-     *            truncated tor
-     * @return
+     *            : truncation upper bound
      */
-    public void fitData(double[] data, String sortby, String dataType, String lower, String upper) {
+    public void fitData(double[] data, String sortby, String dataType,
+	    String lower, String upper) {
 	generator.setVariableName(variableName);
-	double[] samples = generator.fitData(parentFile.getAbsolutePath(), data, simulationNumber, sortby, dataType, lower, upper);
+	double[] samples = generator.fitData(parentFile.getAbsolutePath(),
+		data, simulationNumber, sortby, dataType, lower, upper);
 	distSummary = generator.getDistSummary(); // convert to String
-
+	// updates gui
 	onDistributionGenerated();
 	onFitResultsUpdates();
 	onVariableEnabled();
@@ -290,54 +285,9 @@ public class Model {
     }
 
     /**
+     * generates distribution based on user input
      * 
-     * @param source
-     *            Directory to save image to
-     * @param imgName
-     *            Name of file to save image to (e.g. "testImage.jpg")
-     * @param numRV
-     *            number of random variables to generate
-     * @param distrName
-     *            name of distribution from which random variables would be
-     *            generated from
-     * @param distrParam
-     *            1 X k vector of parameters for the distribution specified
-     *            where k is the number of parameters
-     * @param lower
-     *            lower bound where the generated random variables will be
-     *            truncated tor
-     * @param upper
-     *            upper bound where the generated random variables will be
-     *            truncated tor **list of valid distribution and their
-     *            parameters Distributions with 1 Parameter "Exponential": 'mu'
-     *            - Mean parameter "Poisson": 'lambda' - Mean "Reyleigh": 'b' -
-     *            Defining parameter Distributions with 2 Parameters "Beta": 'a'
-     *            - First shape parameter; 'b' - Second shape parameter
-     *            "Binomial": 'N' - Number of trials; 'p' - Probability of
-     *            success "BirnbaumSaunders": 'beta' - Scale parameter; 'gamma'
-     *            - Shape parameter "ExtremeValue": 'mu' - Location parameter;
-     *            'sigma' - Scale parameter "Gamma": 'a' - Shape parameter; 'b'
-     *            - Scale parameter "InverseGaussian": 'mu' - Scale parameter;
-     *            'lambda' - Shape parameter "Logistic": 'mu' - Mean; 'lambda' -
-     *            Shape parameter "Loglogistic": 'mu' - Mean; 'sigma' - Scale
-     *            parameter "Lognormal": 'mu' - Log mean; 'sigma' - Log standard
-     *            deviation "Nakagami": 'mu' - Shape parameter 'omega' - Scale
-     *            parameter "NegativeBinomial": 'R' - Number of successes; 'p' -
-     *            probability of success "Normal": 'mu' - Mean; 'sigma' -
-     *            Standard deviation "Rician": 's' - Noncentrality parameter;
-     *            'sigma' - Scale parameter "Uniform": 'lower' - Lower
-     *            parameter; 'upper' - Upper parameter "Weibull": 'a' - Scale
-     *            parameter; 'b' - Shape parameter Distributions with 3
-     *            Parameters "Burr": 'alpha' - Scale parameter; 'c' - First
-     *            shape parameter; 'k' - Second shape parameter
-     *            "GeneralizedExtremeValue": 'k' - Shape parameter; 'sigma' -
-     *            Scale parameter; 'mu' - Location parameter
-     *            "GeneralizedPareto": 'k' - Shape parameter; 'sigma' - Scale
-     *            parameter; 'theta' - Location parameter "tLocationScale": 'mu'
-     *            - Location parameter; 'sigma' - Scale parameter; 'nu' -
-     *            Degrees of freedom "Triangular": 'a' - Lower limit; 'b' - Peak
-     *            location; 'c' - Upper limit
-     * 
+     * @param parameters
      */
     public void generateRV(double[] parameters) {
 	double[] samples = makeDistModel.generateRnd(parameters);
@@ -345,39 +295,50 @@ public class Model {
 	randomVariableList.put(variableName, samples);
 	onVariableEnabled();
 	onSamplesUpdated(samples);
+	onDataUpdates();
     }
-    
-    public void generateGraphs(){
+
+    /**
+     * generates graphs
+     */
+    public void generateGraphs() {
 	graphs.setResults();
 	onUpdatedHistoGraphGenerated(graphs.getHistogramCharts());
 	onUpdatedTimeSeriesGraphGenerated(graphs.getTimeSeriesCharts());
-	initializeSenstivityAnalysis();
+	//initializeSenstivityAnalysis();
     }
-    
-    private void initializedGraphGenerator(){
-	graphs = new GraphGenerator(resultFile, simulationNumber, idfData.getValue("RunPeriod", "Start Year"));
-	graphs.setSized(idfData.getValue("SimulationControl", "Run Simulation for Sizing Periods"));
+
+    /**
+     * Below are all utility method that be called within this class for
+     * initialization or acting as helpers or notifying listeners
+     */
+    private void initializedGraphGenerator() {
+	graphs = new GraphGenerator(resultFile, simulationNumber,
+		idfData.getValue("RunPeriod", "Start Year"));
+	graphs.setSized(idfData.getValue("SimulationControl",
+		"Run Simulation for Sizing Periods"));
     }
-    
-    private void initializedIdfData() throws IOException{
+
+    private void initializedIdfData() throws IOException {
 	idfData.setFilePath(eplusFile.getAbsolutePath());
 	idfData.readEplusFile();
 	onReadEplusFile();
     }
-    
-    private void writingFile(EnergyPlusFilesGenerator reader, Integer index, File folder){
+
+    private void writingFile(EnergyPlusFilesGenerator reader, Integer index,
+	    File folder) {
 	Set<String> variable = randomVariableList.keySet();
 	Iterator<String> iterator = variable.iterator();
-	while(iterator.hasNext()){
+	while (iterator.hasNext()) {
 	    String charactor = iterator.next();
 	    Double value = randomVariableList.get(charactor)[index];
 
 	    reader.modifySpecialCharactor(charactor, value.toString());
 	}
-	//write the file
-	reader.WriteIdf(folder.getAbsolutePath(),index.toString());
+	// write the file
+	reader.WriteIdf(folder.getAbsolutePath(), index.toString());
     }
-    
+
     /**
      * Create a file under parent folder that contains all the simulation
      * results
@@ -430,15 +391,16 @@ public class Model {
 	    m.variableEnabled(variableName);
 	}
     }
-    
+
     /**
      * update the model
+     * 
      * @param samples
      */
     private void onSamplesUpdated(double[] samples) {
 	for (MakeDistGraphGeneratorListener mdl : mdGraphs) {
-	    if(mdl.getVariable().equals(variableName)){
-		    mdl.onUpdatedDistGenerated(samples);
+	    if (mdl.getVariable().equals(variableName)) {
+		mdl.onUpdatedDistGenerated(samples);
 	    }
 	}
     }
@@ -448,10 +410,11 @@ public class Model {
      */
     private void onReadEplusFile() {
 	for (LoadIdfListeners l : loadIDFListeners) {
-	    l.loadedEnergyPlusFile(idfData.getVariableList(), idfData.getVaraibleKeySets());
+	    l.loadedEnergyPlusFile(idfData.getVariableList(),
+		    idfData.getVaraibleKeySets());
 	}
     }
-    
+
     /**
      * notify GUI the changes in the time series charts
      */
